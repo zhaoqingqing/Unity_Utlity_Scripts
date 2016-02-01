baec156bfc5816b8f6dd3132add9ae840d1652c8
zhaoqingqing <zhaoqignqing@gmail.com>
2016-02-01 16:40:52 +0800

    Add ResourceChecker .更新ReadMe

---
+++ Assets/Editor/Tools/EditorStyleViewer.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// 查看默认的gui skin样式
+/// </summary>
+public class EditorGUIView : EditorWindow
+{
+	private Vector2 scrollPosition = Vector2.zero;
+	private string search = string.Empty;
+
+	[MenuItem("Tools/默认GUI样式查看器")]
+	static void Init()
+	{
+		var window= EditorWindow.GetWindow<EditorGUIView>();
+		window.title = "GUI样式查看器";
+		window.Show();
+		
+	}
+
+	void OnGUI()
+	{
+		GUILayout.BeginHorizontal("HelpBox");
+		GUILayout.Label("单击左侧样式将复制其名到剪贴板", "label");
+		GUILayout.FlexibleSpace();
+		GUILayout.Label("查找:");
+		search = EditorGUILayout.TextField(search);
+		GUILayout.EndHorizontal();
+
+		scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+
+		//foreach (GUIStyle style in GUI.skin.customStyles)
+		foreach (GUIStyle style in GUI.skin)
+		{
+			//过滤
+			if (style.name.ToLower().Contains(search.ToLower()))
+			{
+				//设置奇偶行不同背景
+				GUILayout.BeginHorizontal("PopupCurveSwatchBackground");
+				GUILayout.Space(20);//左边留白20
+				if (GUILayout.Button(style.name, style))
+				{
+					//把名字存储在剪粘板 
+					EditorGUIUtility.systemCopyBuffer = style.name; // "\"" + style.name + "\"";
+				}
+				GUILayout.FlexibleSpace();
+				EditorGUILayout.SelectableLabel("\"" + style.name + "\"");
+				GUILayout.EndHorizontal();
+				GUILayout.Space(20);//右边留白20
+			}
+		}
+
+		GUILayout.EndScrollView();
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Unity_Editor_Scripts
-Unity 编辑器的一些扩展脚本
+Unity 编辑器的一些扩展脚本，在每个插件的目录中都有对应的功能预览图片(preview结尾)
+##AlignEditor##
+
+工作过程中自己写的一个对齐小插件，方便在开发中对Gameobject进行排列
+
+##UnityLock##
+AssetStore上的一个免费小插件，可以锁定Gameobject不可编辑
+![](https://github.com/zhaoqingqing/Unity_Editor_Scripts/blob/master/Assets/Editor/UnityLock/UnityLock-preview.jpg)
+
+##EditorStyleViewer##
+查看编辑器默认的样式
+
+
+
+## Resource-Checker ##
+
+出处：[https://github.com/handcircus/Unity-Resource-Checker](https://github.com/handcircus/Unity-Resource-Checker)
+
+需要Unity4.6及更高的版本
